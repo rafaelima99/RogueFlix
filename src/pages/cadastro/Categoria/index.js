0802d5d1d50c3c0522f7d9042ecf71de02f5edab
rafaelima99 <43426335+rafaelima99@users.noreply.github.com>
2020-08-09 12:18:20 -0500

    fix URL Heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,7 +28,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes("localhost")) {
-      const URL = "http://localhost:8080/categorias";
+      const URL = window.location.hostname.includes("localhost")
+        ? "http://localhost:8080/categorias"
+        : "https://rogue-flix.herokuapp.com/categorias";
       fetch(URL).then(async (respostaDoServer) => {
         if (respostaDoServer.ok) {
           const resposta = await respostaDoServer.json();
